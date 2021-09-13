f695e27659a4c1583ac22d0ed33f97e2ff24ddb0
ciceromngr <cicerotransformice@gmail.com>
2021-09-13 10:51:17 -0300

    first commit

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express'
+import UserController from './app/controller/UserController'
 
 const app = express()
+
+app.post('/users', UserController.store)
 
 app.listen(3333, () => console.log("server running on localhost: 3333"))
